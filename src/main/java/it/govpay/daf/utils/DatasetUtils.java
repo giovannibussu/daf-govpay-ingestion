e56e916975b5b8892ef5076bd9a196b526e4041a
Lorenzo Nardi <nardi@link.it>
2017-10-07 17:56:43 +0200

    Primmo test

---
+++ src/main/java/it/govpay/daf/utils/DatasetUtils.java
@@ -0,0 +1,17 @@
+package it.govpay.daf.utils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class DatasetUtils 
+{
+    public static void rt_xml2json( List<byte[]> rtxml, OutputStream rtjson) throws IOException
+    {
+    	rtjson.write("TODO".getBytes());
+    }
+}
